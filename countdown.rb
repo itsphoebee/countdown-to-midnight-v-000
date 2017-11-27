92c88035adb73d4a104eeadf9dc3d7b7b829c6ea
Phoebe Lau <phoebelau7@gmail.com>
2017-11-27 22:21:50 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,11 @@ def countdown(n)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep 5
+def countdown_with_sleep(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -=1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
