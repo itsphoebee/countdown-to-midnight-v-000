6aaac4f12a2cfb73a0ff32ab1d799e0b37b09ff8
Phoebe Lau <phoebelau7@gmail.com>
2017-11-27 22:08:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(input)
-  input = gets.to_i 
-  until input == 0 
+  input = gets.to_i
+  until input == 0
     puts "#{input} SECOND(S)!"
     sleep 1
     input -= 1
