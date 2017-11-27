8a76c664fdd4bf88aea8c41b077f5b8089a636d4
Phoebe Lau <phoebelau7@gmail.com>
2017-11-27 22:08:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(input)
+  input = gets.to_i 
+  until input == 0 
+    puts "#{input} SECOND(S)!"
+    sleep 1
+    input -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
